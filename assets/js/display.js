2bf746dc98c2dff92fa8eb1f801e74e4acffb2ae
함인규 <hfire1207@gmail.com>
2025-05-25 18:30:49 +0900

    FIX :: QR코드 사이즈 변경 방법 수정

--- assets/js/display.js
+++ assets/js/display.js
@@ -1,11 +1,7 @@
 const $main = document.querySelector('main')
 const channel = client.channel('test-channel');
 
-new QRCode(document.querySelector("#qrcode"), {
-    text: location.origin,
-    width: 128,
-    height: 128
-});
+new QRCode(document.querySelector("#qrcode"), location.origin);
 
 // on(구독할 기능, 설정객체, callback)
 channel.on(
